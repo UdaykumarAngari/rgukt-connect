0f51dbb29c21fb0d324dc7431b80a8cc4827b160
UdaykumarAngari <udaykumaruk5632@gmail.com>
2026-05-26 17:30:47 +0530

    backend folder structure

---
+++ backend/src/main/java/com/uday/rgukt_connect/RguktConnectApplication.java
@@ -0,0 +1,13 @@
+package com.uday.rgukt_connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RguktConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RguktConnectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/uday/rgukt_connect/RguktConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uday.rgukt_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RguktConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
