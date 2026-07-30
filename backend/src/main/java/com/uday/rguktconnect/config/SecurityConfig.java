ee42e88ee7b3b9158bc7ffcd83645f9580cdd67a
UdaykumarAngari <udaykumaruk5632@gmail.com>
2026-07-30 11:47:23 +0530

    update jenkins file : pull image from docker hub

--- backend/src/main/java/com/uday/rguktconnect/config/SecurityConfig.java
+++ backend/src/main/java/com/uday/rguktconnect/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/health").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/ws-chat/**").permitAll()
                         .anyRequest().authenticated()
